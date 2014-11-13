5523ce8184603abed1a63c11b680142f357d2d0a
TanvirAmi <tanvir.focus@gmail.com>
2014-11-13 08:33:44 +0600

    first commit

---
+++ blank_tab_hook.php
@@ -0,0 +1,43 @@
+<?php
+/**
+* Plugin Name: TJ Blank Tab
+* Plugin URI: https:
+* Description: This plugin will add target="_blank" functionality, which open the link with a new window(Tab).
+* Version: 1.0
+* Author: Theme Junkie
+* Author URI: http://www.theme-junkie.com/
+* Author Email: tanvir.focus@gmail.com
+*
+* This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+* General Public License as published by the Free Software Foundation; either version 2 of the License,
+* or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+* even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* You should have received a copy of the GNU General Public License along with this program; if not, write
+* to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*
+*/
+
+
+//Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class blank_tab{
+	//calling constructor method
+	public function __construct(){
+		add_filter('get_comment_author_link', array($this, 'links_in_new_tab'));
+		add_filter('comment_text', array($this, 'links_in_new_tab'));
+		add_filter('the_content', array($this, 'links_in_new_tab'));
+	}
+
+	//functionality method
+	public function links_in_new_tab($x){
+		$link = str_replace('<a', '<a target="_blank"', $x);
+		return $link;
+	}
+}
+
+$obj= new blank_tab;
+?>
---
+++ readme.txt
@@ -0,0 +1,36 @@
+== TJ Blank Tab ==
+Contributors: Tanvir, themejunkie
+Tags: blank tab, blank window, external-links, links, new tab, new window, tabs, target blank, target new
+Requires at least: 3.6
+Tested up to: 4.0
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+
+==Description==
+
+This plugin will add target="_blank" functionality in comment and post content, which open the link with a new window(Tab).
+
+==Installation==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload `TJ Blank Tab` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3.You have done.
+
+= Support =
+
+* Get [support](http://www.theme-junkie.com/forum/)
+
+= Plugin Info =
+* Developed by [Tanvir](https://twitter.com/TanvirFocus) & [Theme Junkie](http://www.theme-junkie.com/)
+* Check out the [Github](https://github.com/TanvirAmi//) repo to contribute.
+
+==Changelog==
+
+Version: 1.0
+* Initial Release
